9fed73eb5ec84ad89ab6ca7f18c5450acc48fe7c
azrakgn <akgunazra2004@gmail.com>
2025-11-30 22:00:54 +0300

    Initial commit...

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+ public class Main{
+     public static final int MONTHS =12;
+     public static final int DAYS   =28;
+     public static final int COMMODITY  =5;
+
+     public static String [] commodities = {"Gold","Oil", "Silver", "Wheat" , "Copper" };
+     public static String []  months = { "January","February","March","April","May","June","July","August","September","November", "December" };
+
+
+     }
